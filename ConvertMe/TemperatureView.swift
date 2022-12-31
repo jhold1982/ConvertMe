7e465438663c2ef38a67381929792c0b76776c99
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-12-31 16:16:45 -0600

    first commit

---
+++ ConvertMe/TemperatureView.swift
@@ -0,0 +1,20 @@
+//
+//  TemperatureView.swift
+//  ConvertMe
+//
+//  Created by Justin Hold on 12/31/22.
+//
+
+import SwiftUI
+
+struct TemperatureView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TemperatureView_Previews: PreviewProvider {
+    static var previews: some View {
+        TemperatureView()
+    }
+}
