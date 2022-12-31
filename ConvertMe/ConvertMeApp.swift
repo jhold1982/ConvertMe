b4e1711a59fe53d6036b99d111260c569dc5a867
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2022-12-31 16:12:33 -0600

    Initial Commit

---
+++ ConvertMe/ConvertMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertMeApp.swift
+//  ConvertMe
+//
+//  Created by Justin Hold on 12/31/22.
+//
+
+import SwiftUI
+
+@main
+struct ConvertMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
