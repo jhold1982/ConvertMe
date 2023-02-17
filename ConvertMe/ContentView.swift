679cf1ff8f282e4cdb6b733b03d864c022e2c1da
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-02-17 14:46:25 -0600

    App redisgn, updated properties for unit conversion

--- ConvertMe/ContentView.swift
+++ ConvertMe/ContentView.swift
@@ -8,34 +8,108 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State private var input = 0.0
+	@State private var selectedUnits = 0
+	@State private var inputUnit: Dimension = UnitTemperature.fahrenheit
+	@State private var outputUnit: Dimension = UnitTemperature.celsius
 	
-    var body: some View {
-		TabView {
-			HomeView()
-				.tabItem {
-					Label("Home", systemImage: "house")
+	
+	@FocusState private var inputIsFocused: Bool
+	
+	let formatter: MeasurementFormatter
+	
+	let conversions = ["Distance", "Mass", "Temperature", "Time"]
+	
+	let unitTypes = [
+		[UnitLength.feet, UnitLength.kilometers, UnitLength.meters,
+		 UnitLength.miles, UnitLength.yards],
+		
+		[UnitMass.grams, UnitMass.kilograms, UnitMass.ounces,
+		 UnitMass.pounds],
+		
+		[UnitTemperature.celsius, UnitTemperature.fahrenheit,
+		 UnitTemperature.kelvin],
+		
+		[UnitDuration.hours, UnitDuration.minutes, UnitDuration.seconds]
+	]
+	
+	var result: String {
+		let inputMeasurement = Measurement(value: input, unit: inputUnit)
+		let outputMeasurement = inputMeasurement.converted(to: outputUnit)
+		return formatter.string(from: outputMeasurement)
+	}
+	
+	var body: some View {
+		NavigationStack {
+			Form {
+				
+				Section("Amount to convert") {
+					TextField("Amount", value: $input, format: .number)
+						.keyboardType(.decimalPad)
+						.focused($inputIsFocused)
 				}
-				.tag(0)
-			TemperatureView()
-				.tabItem {
-					Label("Temperatures", systemImage: "medical.thermometer.fill")
+				
+				Picker("Conversion", selection: $selectedUnits) {
+					ForEach(0..<conversions.count, id: \.self) {
+						Text(conversions[$0])
+					}
 				}
-				.tag(1)
-			LiquidView()
-				.tabItem {
-					Label("Liquids", systemImage: "drop.circle.fill")
+				.pickerStyle(.menu)
+				
+				Picker("Convert from:", selection: $inputUnit) {
+					ForEach(unitTypes[selectedUnits], id: \.self) {
+						Text(formatter.string(from: $0).capitalized)
+					}
 				}
-				.tag(2)
-			WeightsView()
-				.tabItem {
-					Label("Weights", systemImage: "scalemass.fill")
+				.pickerStyle(.menu)
+				
+				Picker("Convert to:", selection: $outputUnit) {
+					ForEach(unitTypes[selectedUnits], id: \.self) {
+						Text(formatter.string(from: $0).capitalized)
+					}
 				}
-			DistanceView()
-				.tabItem {
-					Label("Distances", systemImage: "arrow.left.and.right")
+				.pickerStyle(.menu)
+				
+				Section("Result") {
+					Text(result)
 				}
+				
+			}
+			.navigationTitle("ConvertMe")
+			.toolbar {
+				ToolbarItem {
+					Button("Reset All") {
+						reset()
+					}
+					.padding()
+				}
+			}
+			.toolbar {
+				ToolbarItemGroup(placement: .keyboard) {
+					Spacer()
+					Button("Done") {
+					inputIsFocused = false
+					}
+				}
+			}
+			.onChange(of: selectedUnits) { newSelection in
+				let units = unitTypes[newSelection]
+				inputUnit = units[0]
+				outputUnit = units[1]
+			}
 		}
-    }
+	}
+	
+	init() {
+		formatter = MeasurementFormatter()
+		formatter.unitOptions = .providedUnit
+		formatter.unitStyle = .short
+	}
+	
+	func reset() {
+		input = 0.0
+		selectedUnits = 0
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
